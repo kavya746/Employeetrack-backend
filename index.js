3f4b936c2f9bc5457f9f8599291da4d1d8a76bea
Kavya <thotakavya2005@gmail.com>
2025-05-16 19:56:05 +0530

    initial backend commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import authRouter from './routes/auth.js'
+import departmentRouter from './routes/department.js'
+import employeeRouter from './routes/employee.js'
+import salaryRouter from './routes/salary.js'
+import leaveRouter from './routes/leave.js'
+import settingRouter from './routes/setting.js';
+import dashboardRouter from './routes/dashboard.js';
+
+import userRoutes from './routes/auth_pass.js' //password
+
+import connectToDatabase from './db/db.js'
+
+connectToDatabase()
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRouter)
+app.use(express.static('public/uploads'))
+app.use('/api/user', userRoutes) //password
+
+app.use('/api/department', departmentRouter);
+app.use('/api/employee', employeeRouter);
+app.use('/api/salary', salaryRouter);
+app.use('/api/leave', leaveRouter);
+app.use('/api/setting', settingRouter);
+app.use('/api/dashboard', dashboardRouter);
+
+app.listen(process.env.PORT, () =>{
+    console.log(`server is running on port ${process.env.PORT}`)
+})
---
+++ routes/auth_pass.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+
+// Forgot Password
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.json({ message: "user not registered" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: "5m" });
+    const encodedToken = encodeURIComponent(token).replace(/\./g, "%2E");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "kavyathota03@gmail.com",       // Replace with your actual email
+        pass: "lywx ptxn wntw hxlf", // Replace with real app password
+      },
+    });
+
+    const mailOptions = {
+      from: "your email@gmail.com",
+      to: email,
+      subject: "Reset Password",
+      text: `http://localhost:5173/resetPassword/${encodedToken}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) return res.json({ message: "error sending email" });
+      return res.json({ status: true, message: "email sent" });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Reset Password
+router.post("/resetPassword/:token", async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const id = decoded.id;
+    const hashPassword = await bcrypt.hash(password, 10);
+    await User.findByIdAndUpdate({ _id: id }, { password: hashPassword });
+
+    return res.json({ status: true, message: "password updated" });
+  } catch (err) {
+    return res.json("invalid token");
+  }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  return res.json({ status: true });
+});
+
+export default router;
