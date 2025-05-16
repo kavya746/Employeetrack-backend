20436c6c62bbbeacff698be9ddc6cda55d544e46
Kavya <thotakavya2005@gmail.com>
2025-05-16 20:27:37 +0530

    url change

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ import connectToDatabase from './db/db.js'
 
 connectToDatabase()
 const app=express()
-app.use(cors())
+app.use(cors({
+    origin : "https://employeetrack-frontend-ten.vercel.app",
+    credentials: true
+}))
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use(express.static('public/uploads'))
